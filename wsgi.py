736d9aaf8663b77f4a4903f440879f652de05c4c
Hasan Faraaz <hasanandroidapp@gmail.com>
2020-01-24 13:02:33 +0530

    Update wsgi.py
    added time

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 from flask import Flask
 application = Flask(__name__)
+import time
 
 @application.route("/")
 def hello():
     return "Hello World! 2"
+
+@application.route("/time")
+def hello():
+    return time.time()
 
 if __name__ == "__main__":
     application.run()
