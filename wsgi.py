9389f1da13e1dfacaf7acccf4cc73e8502a4ea8d
Hasan Faraaz <hasanandroidapp@gmail.com>
2020-01-24 13:24:48 +0530

    Update wsgi.py
    str

--- wsgi.py
+++ wsgi.py
@@ -2,13 +2,13 @@ from flask import Flask
 application = Flask(__name__)
 
 import sys
-import datetime;
+import datetime
 
 
 @application.route("/")
 def hello():
     ts = datetime.datetime.now().timestamp()
-    return(ts)
+    return(str(ts))
     
     #return "<-->".join(sys.version, "hello")
 
